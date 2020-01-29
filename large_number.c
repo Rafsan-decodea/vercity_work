26ebd57c04cc9c194b9a65779c817d6d2a1c8284
Rafsan-decodea <kalilinux609@example.com>
2020-01-29 14:46:47 +0600

    This is

--- large_number.c
+++ large_number.c
@@ -1,25 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int calculate(int a ,int b ,int c , int d)
 {
-  printf("--------------------------------------------");
-  if (a>b && a>c && a>d){printf("Grater number is A");}
-  else if(b>a && b>c && b>d){printf("Grater number id B");}
-  else if(c>a && c>d && c>b){printf("Grater number is C");}
-  else{printf("Grater number is D" );}
+  printf("--------------------------------------------\n");
+  if (a>b && a>c && a>d){printf("Grater number is A ==>%d",a);}
+  else if(b>a && b>c && b>d){printf("Grater number id B==>%d",b);}
+  else if(c>a && c>d && c>b){printf("Grater number is C==>%d",c);}
+  else{printf("Grater number is D==>%d",d );}
 }
 
-int main()
+int main(int argc , char *argv[])
 {
 
 	int a,b,c,d;
-	printf("Enter number===>");
-	scanf("%d",&a);
-	printf("Enter nymber===>");
-	scanf("%d",&b);
-	printf("Enter a number==>");
-	scanf("%d",&c);
-	printf("Enter a number===>");
-	scanf("%d",&d);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = atoi(argv[3]);
+	d =  atoi(argv[4]);
 	calculate(a,b,c,d);
 }
