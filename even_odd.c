a85d054cb0d6f7405cf2d9fff43b05d1dad52f6a
Rafsan-decodea <kalilinux609@example.com>
2020-01-30 14:41:07 +0600

    edit

--- even_odd.c
+++ even_odd.c
@@ -1,15 +1,4 @@
 #include<stdio.h>
 /*  Even OOD */
-int calulate(int number)
-{
-    if (number%2==0)
-    {
-        printf("%d number is even",number);
-    }
-    else
-    {
-        printf("%d number is odd",number);
-    }
-}
-
-int main(){ printf("input a number for check====>");int number ; scanf("%d",&number); calulate(number) ; }
+int calulate(int number){puts(number%2==0?"EVEN":"ODD");}
+int main(){ int number ; printf("input a number for check====>"); scanf("%d",&number); calulate(number) ; }
