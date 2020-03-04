872f0bacbc19da38acdf582959997162d9a21529
Your Name <you@example.com>
2020-03-05 01:02:25 +0600

    add code5

---
+++ lab8/code_0x005.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void calculate(int x, int y){int sum = pow(y,x);printf("\n The Power x in y is===>%d",sum);}
+int main()
+{
+	int a,b; 
+	printf("Enter x number ===>");scanf("%d",&a);
+	printf("Enter y number ==> ");scanf("%d",&b);
+	calculate(a,b);
+}
