d876d20842186a45f62a1e282ca0e21007cfd4c0
Your Name <you@example.com>
2020-10-17 15:18:49 +0600

    add pdf in file

--- file_work_in_c/work.c
+++ file_work_in_c/work.c
@@ -8,7 +8,8 @@ int main()
    char  father_name[100] ;
    char  mother_name[100];
    char age[100] ;
-   file = fopen("F:/c++/t.txt","w");
+   char gpa[100];
+   file = fopen("F:/c++/StdRec.txt","w");
    printf("[+]Your Name is ===>");
    gets(name);
    printf("[+]Your age ===>");
@@ -17,11 +18,13 @@ int main()
    gets(father_name);
    printf("[+]Your Mother Name==>");
    gets(mother_name);
+   printf("[+]Your GPA===>");
+   gets(gpa);
    char margine[200]="-------------About My Self----------";
    fprintf(file, "%s\n",margine);
-   fprintf(file,"Name :\t%s\nAge:\t %s\nFatherName:\t%s\nMother Name:\t%s\n",name,age,father_name,mother_name);
+   fprintf(file,"Name :\t%s\nAge:\t %s\nFatherName:\t%s\nMother Name:\t%s\nYour GPA is:\t%s\n",name,age,father_name,mother_name,gpa);
    printf("\n\nFiles Written is Success!!!!!!!!!");
-   system("explorer t.txt");
+   system("explorer StdRec.txt");
    fclose(file);
     
 }
