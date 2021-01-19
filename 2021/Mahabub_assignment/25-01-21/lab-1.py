8670da9450cd7502816d2bae0fbd5a23f6118798
Your Name <you@example.com>
2021-01-19 19:32:56 +0600

    lab-1

--- 2021/Mahabub_assignment/25-01-21/lab-1.py
+++ 2021/Mahabub_assignment/25-01-21/lab-1.py
@@ -4,8 +4,33 @@ def implement_stack(data):
      list = []
      for x in data:
         list.append(x)
+     if list:
+        def push_pop():
+          print ("1) Press One For pop")
+          print ("2) Press Two For Push")
+          print ("---------------------")
+          choose  = input("Chosse===>")
+          if choose == 1:
+             data = input("input index For pop From Stack===>")
+             list.pop(data)
+             print list
+             push_pop()
+          elif choose==2:
+            data = input("input Any thing in Stack===> ")
+            list.append(data)
+            print ("[++] After Drop Your {} index ===>".format(data,list))
+            push_pop()
+          else:
+            print("[???] input Valide Choose")
+            push_pop()
+     push_pop()
+
 
 
 if __name__ == "__main__":
-    data = raw_input("[+] input data in to the Stack(Use Commad)====>".split(","))
-    implement_stack(data)
+    data = input("[+] input data in to the Stack(Use Commad)====>")
+    try:
+     implement_stack(data)
+    except:
+        print ("[!!!!] Your Stack Run into under Flow ")
+        implement_stack(data)
