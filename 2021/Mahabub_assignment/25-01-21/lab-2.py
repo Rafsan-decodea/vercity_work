17861b0e6e97cb1a84c844834bacaceb9374da64
Your Name <you@example.com>
2021-01-20 01:15:59 +0600

    final-lab1_modify

--- 2021/Mahabub_assignment/25-01-21/lab-2.py
+++ 2021/Mahabub_assignment/25-01-21/lab-2.py
@@ -1,9 +1,26 @@
 
+print ("=============++++POSTFIX Operation========____++_\n")
 
-def postfix():
-    pass
+def postfix(data):
+     stack = []
+     number = []
+     degite = []
+     index = []
+     for x in data:
+         stack.append(x)
+     for y in range(0,len(stack)):
+             if stack[y].isdigit():
+                number.append(stack[y])
+             else:
+                degite.append(stack[y])
+                index.append(stack.index(stack[y]))
+     print number
+     print degite
+     print index
+
 
 
 
 if __name__ =="__main__":
-    postfix()
+    data = raw_input("input Postfix Formate for Calculate==>").split(' ')
+    postfix(data)
