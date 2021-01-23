310e816317a21d5298a1918eac08758812bf4a3f
Your Name <you@example.com>
2021-01-24 00:30:25 +0600

    Final Lab-2

--- 2021/Mahabub_assignment/25-01-21/lab-2.py
+++ 2021/Mahabub_assignment/25-01-21/lab-2.py
@@ -4,8 +4,7 @@ def postfix(data):
      stack = []
      for x in data:
          stack.append(x)
-     for y in range(len(stack)):
-             if stack[y].isdigit() !=True :
+     for y in range(0,len(stack)):
                 if stack[y] == "*":
                       temp = stack[y-1]
                       temp1= stack[y-2]
@@ -15,11 +14,18 @@ def postfix(data):
                       stack.remove(temp)
                       stack.remove(temp1)
                       print (stack)
-                elif stack[y] =="+":
-                    pass
-
-
-
+                      break
+     for x in range(len(stack)):
+             if "+" in stack[x]:
+                 temp = stack[x-1]
+                 temp1 = stack[x+1]
+                 data= int(temp)+int(temp1)
+                 stack.remove(stack[x])
+                 stack.remove(temp)
+                 stack.remove(temp1)
+                 stack.append(data)
+                 print ("[+] Ans is ==>{}".format(stack))
+                 break
 
 
 if __name__ =="__main__":
