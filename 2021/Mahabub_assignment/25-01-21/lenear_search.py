c193c8079a7c94b1c1b94eb42c7201eaf15d70ea
Your Name <you@example.com>
2021-01-24 17:28:41 +0600

    leaner_data_added

--- 2021/Mahabub_assignment/25-01-21/lenear_search.py
+++ 2021/Mahabub_assignment/25-01-21/lenear_search.py
@@ -5,15 +5,17 @@ def leaner_search(data):
     for x in data:
         list.append(x)
     print ("[##] data is recorded .. Now Search Data in Record")
-    search_data = input("[+]Input for Search data in record==>")
-    for x in list:
-        print x
+    def run():
+     print("-"*10)
+     search_data = raw_input("[+]Input for Search data in record==>")
+     for x in list:
         if search_data in x:
            print ("[+] Your input ({}) data is found in ({}) this Record ".format(search_data,x))
         else:
            print("[!!!] Your input ({}) data Is NOT found in ({}) this recoderd ".format(search_data,x))
+    while 1: run()
 
 if __name__ == "__main__":
    data = raw_input("[+]input data For Store ===>").split(',')
+
    leaner_search(data)
-   #asdasd
