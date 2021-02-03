e74f3f34f3c1b062c9a426cf55423f32300f9aea
Your Name <you@example.com>
2021-02-04 02:29:41 +0600

    logine Mathod Success

--- 2021/Mahabub_assignment/Stumanagment/db.java
+++ 2021/Mahabub_assignment/Stumanagment/db.java
@@ -5,7 +5,7 @@ import java.sql.*;
 class  db{
 
     private Connection  conn;
-    public  Statement statement;
+   // public  Statement statement;
     public ResultSet result;
     void  stu_login(String username,String password)
      {
@@ -13,20 +13,21 @@ class  db{
 
           Class.forName("com.mysql.cj.jdbc.Driver");  
           conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/java_project","root","");  
-          statement=conn.createStatement(); 
-          result =statement.executeQuery("select * from stu_users");  
-         while( result.next())
-         {
-          System.out.println(result.getString("username"));
-          System.out.println(result.getString("password"));
-            if (result.getString("username").toString()==username && result.getString("password")==password)
-            {
-              System.out.print("login");
-            } 
-            else{
-              System.out.print("False");
-                }  
-            }
+          
+          String sql = "SELECT * FROM stu_users WHERE username = ? and password = ?";
+          PreparedStatement statement = conn.prepareStatement(sql);
+          statement.setString(1, username);
+          statement.setString(2, password);
+          ResultSet result = statement.executeQuery();
+          if(result.next())
+          {
+          	System.out.print("login");
+          }
+          else
+          {
+          	System.out.print("Not LOgin");
+          }
+
         }      
         catch(Exception e){ 
 
