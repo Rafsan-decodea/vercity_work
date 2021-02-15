13e9e2e259215c7635b85e2e29cc23b03031a6f9
Your Name <you@example.com>
2021-02-15 18:06:51 +0600

    fix Debagging

--- 2021/Mahabub_assignment/Stumanagement/teacher.java
+++ 2021/Mahabub_assignment/Stumanagement/teacher.java
@@ -36,7 +36,7 @@ class  teacher extends index
     {
         System.out.println("\n[+] ===== Welcome to teacher Panel =========\n");
         System.out.println("1) See Student Details ");
-        System.out.println("2) See All Studnet ");
+        System.out.println("2) See Total Student Count ");
         System.out.println("3) Delete Studnet From Entry");
         System.out.println("4) See Your information");
         System.out.println("5) Update Your information");
@@ -57,9 +57,16 @@ class  teacher extends index
                         input.nextLine();
                         String stu_id = input.nextLine();
                         if(stu_register_array.containsKey(stu_id))
-                             {
-            
+                             {  
+                           
                                 ArrayList<String> stu_has_next = stu_register_array.get(stu_id);
+                                if(2>=stu_has_next.size())
+                                {
+                                    System.out.print("\n[!!!] Your Enter id's is ("+stu_id+") Whitch information Not ablavel Expect User id("+stu_has_next.get(0)+") And password ("+stu_has_next.get(1)+") \n\n");
+                                    teacher_panal(id);
+                                }
+                                else{    
+                                
                                 System.out.println("[+] Your name is ===> "+stu_has_next.get(2));
                                 System.out.println("[+] Your Father Name ===>"+stu_has_next.get(3));
                                 System.out.println("[+] Your Mother Name ===> "+stu_has_next.get(4));
@@ -67,6 +74,8 @@ class  teacher extends index
                                 System.out.println("[+] Your Birth Day ===>"+stu_has_next.get(6));
                                 System.out.println("[+] Your Id is ===>"+stu_has_next.get(0));
                                 System.out.print("[+] Your Password is ===>"+stu_has_next.get(1));
+                               
+                                }
                                 teacher_panal(id);
 
                               }
@@ -87,10 +96,31 @@ class  teacher extends index
 
             else if(chooise==2)
             {
-
+                System.out.print("\n\n[+]You Have in total "+stu_register_array.size()+" in your entry And There Id's Are==> ");
+                System.out.print(stu_register_array.keySet()+"\n\n");
+                teacher_panal(id);
+                
             }
             else if(chooise==3)
             {
+               System.out.print("[+] Enter Id number of Student ===>");
+              input.nextInt();
+              String remove_id = input.nextLine();
+              if (stu_register_array.containsKey(remove_id))
+              {
+                System.out.println(stu_register_array);
+                stu_register_array.remove(remove_id);
+                System.out.print("\nn[+] Succesfully Remove this Studnet \n\n");
+                System.out.println(stu_register_array);
+                teacher_panal(id);
+
+              }
+              else
+              {
+                System.out.print("\nn[+] Your Enter Studnet id Not Exist \n\n");
+                teacher_panal(id);
+
+              }
                
             }
             else if(chooise==4)
